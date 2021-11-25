877b92fcc2953dc05ecccd4a648f98e8b72dee47
Gustavo Wagner <gustavorwagner@gmail.com>
2021-11-25 12:37:56 -0300

    Adition of new DB

--- engsoft2.sql
+++ engsoft2.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 17-Nov-2021 às 23:08
+-- Tempo de geração: 25-Nov-2021 às 16:37
 -- Versão do servidor: 10.4.20-MariaDB
 -- versão do PHP: 8.0.8
 
@@ -20,6 +20,8 @@ SET time_zone = "+00:00";
 --
 -- Banco de dados: `engsoft2`
 --
+CREATE DATABASE IF NOT EXISTS `engsoft2` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `engsoft2`;
 
 -- --------------------------------------------------------
 
@@ -33,16 +35,59 @@ CREATE TABLE `clientes` (
   `endereco` varchar(255) NOT NULL,
   `telefone` int(11) DEFAULT NULL,
   `celular` int(11) DEFAULT NULL,
-  `cpf` varchar(12) NOT NULL
+  `cpf` varchar(14) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+-- --------------------------------------------------------
+
 --
--- Extraindo dados da tabela `clientes`
+-- Estrutura da tabela `compras`
 --
 
-INSERT INTO `clientes` (`id`, `nome`, `endereco`, `telefone`, `celular`, `cpf`) VALUES
-(2, 'Angelo Salvador', 'Camburi', 33339999, 999899999, '12365478900'),
-(4, 'Cristiano Biancardi', 'iaugbai 654984', 32165487, 987654321, '65498732180');
+CREATE TABLE `compras` (
+  `id` int(11) NOT NULL,
+  `vendedor` int(11) NOT NULL,
+  `cliente` int(11) NOT NULL,
+  `produto` int(11) NOT NULL,
+  `valor` float NOT NULL,
+  `valorDesconto` float NOT NULL,
+  `valorTotal` float NOT NULL,
+  `parcelas` int(11) NOT NULL,
+  `valorParcela` float NOT NULL,
+  `formaPagamento` char(1) NOT NULL,
+  `dtVenda` date NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `produtos`
+--
+
+CREATE TABLE `produtos` (
+  `id` int(11) NOT NULL,
+  `descricao` varchar(255) NOT NULL,
+  `valor` float NOT NULL,
+  `qtdEstoque` int(11) NOT NULL,
+  `estoqueMinimo` int(11) NOT NULL,
+  `validade` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `vendedores`
+--
+
+CREATE TABLE `vendedores` (
+  `id` int(11) NOT NULL,
+  `matricula` varchar(11) NOT NULL,
+  `nome` varchar(255) NOT NULL,
+  `endereco` varchar(255) NOT NULL,
+  `telefone` int(11) NOT NULL,
+  `cpf` varchar(14) NOT NULL,
+  `salarioBase` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Índices para tabelas despejadas
@@ -55,6 +100,25 @@ ALTER TABLE `clientes`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Índices para tabela `compras`
+--
+ALTER TABLE `compras`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices para tabela `produtos`
+--
+ALTER TABLE `produtos`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices para tabela `vendedores`
+--
+ALTER TABLE `vendedores`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `vendedor_matricula` (`matricula`);
+
+--
 -- AUTO_INCREMENT de tabelas despejadas
 --
 
@@ -62,7 +126,25 @@ ALTER TABLE `clientes`
 -- AUTO_INCREMENT de tabela `clientes`
 --
 ALTER TABLE `clientes`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `compras`
+--
+ALTER TABLE `compras`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `produtos`
+--
+ALTER TABLE `produtos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `vendedores`
+--
+ALTER TABLE `vendedores`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
